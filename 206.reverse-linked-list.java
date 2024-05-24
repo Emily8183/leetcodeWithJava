beecfbc5cef0d4b17863b69739104b74289dd485
Emily <emily.zy.lin@gmail.com>
2024-05-24 14:37:14 -0500

    finish lc206

---
+++ 206.reverse-linked-list.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=206 lang=java
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    
+    public ListNode reverseList(ListNode head) {
+        return reverse (null,head);
+        //注意这边的顺序，pre=null, cur=head; 
+    }
+
+    private ListNode reverse (ListNode pre, ListNode cur) {
+
+        if (cur == null) {
+            return pre;
+        }
+
+        ListNode temp = cur.next;
+      
+        cur.next = pre;
+        return reverse(cur,temp);
+    }
+        
+    
+}
+// @lc code=end
+
