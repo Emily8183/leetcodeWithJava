dcdc53bbacb90110b78c39d7abb5c59bb64236c1
Emily <emily.zy.lin@gmail.com>
2024-05-29 17:31:56 -0500

    finish lc383

--- 454.4-sum-ii.java
+++ 454.4-sum-ii.java
@@ -7,7 +7,23 @@
 // @lc code=start
 class Solution {
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        int count = 0;
+
+        Map<Integer, Integer> map = new HashMap<Integer,Integer>();
         
+        for (int i: nums1){
+            for (int j:nums2){
+                int sums = i+j;
+                map.put(sums, map.getOrDefault(sums, 0)+1);
+            }
+        }
+
+        for (int i: nums3){
+            for (int j: nums4) {
+                count += map.getOrDefault(0-i-j,0);
+            }
+        }
+        return count;
     }
 }
 // @lc code=end
