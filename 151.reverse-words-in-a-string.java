bc4b50501f7cc6b53e1982dff07f8b6e7e022acc
Emily <emily.zy.lin@gmail.com>
2024-06-13 11:21:53 -0500

    finish lc232

--- 151.reverse-words-in-a-string.java
+++ 151.reverse-words-in-a-string.java
@@ -2,11 +2,22 @@
  * @lc app=leetcode id=151 lang=java
  *
  * [151] Reverse Words in a String
+ * 
+ * //我的思路是先把句子中的词split成item，比如" hello world", 切成["hello", "world"]
+
+第二步是通过双指针反转，变成["world"，"hello"]
+
+第三步是加入空格，恢复成一句句子， 变成 world hello
+
+这道题还有一个方法是把整句句子颠倒过来，然后再把每个单词颠倒过来
+
+//这道题先放一放，以后再做
  */
 
 // @lc code=start
 class Solution {
     public String reverseWords(String s) {
+  
         
     }
 }
