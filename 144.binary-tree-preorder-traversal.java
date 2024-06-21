9c824ca841a5cee250c345b2c7283544a028ad8c
Emily <emily.zy.lin@gmail.com>
2024-06-21 11:15:57 -0500

    finish lc144

--- 144.binary-tree-preorder-traversal.java
+++ 144.binary-tree-preorder-traversal.java
@@ -22,7 +22,19 @@
  */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        
+        List<Integer> result = new ArrayList<>();
+        preorder(root,result);
+        return result;    
+    }
+
+    public void preorder(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+
+        list.add(root.val);
+        preorder(root.left,list);
+        preorder(root.right,list);
     }
 }
 // @lc code=end
