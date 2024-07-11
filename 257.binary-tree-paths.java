6dbe87d0cb86979d6534c8e34e9512397b5cb701
Emily <emily.zy.lin@gmail.com>
2024-07-10 19:53:02 -0500

    add extra notes to lc257

--- 257.binary-tree-paths.java
+++ 257.binary-tree-paths.java
@@ -30,14 +30,15 @@ class Solution {
                 if (root == null) {
                     return res;
                 }
-                List<Integer> paths = new ArrayList<>();// 作为结果中的路径
+                List<Integer> paths = new ArrayList<>();// 每单条路径都对应一个列表，存到 paths 中，再将paths 从array转为string，存到 res 中
+
                 traversal(root, paths, res);
                 return res;
             }
         
             private void traversal(TreeNode root, List<Integer> paths, List<String> res) {
                 paths.add(root.val);// 前序遍历，中
-                // 遇到叶子结点
+                // 遇到叶子结点时，就可以把单条路径加到结果列表res中，return
                 if (root.left == null && root.right == null) {
                     // 输出
                     StringBuilder sb = new StringBuilder();// StringBuilder用来拼接字符串，速度更快
@@ -52,7 +53,7 @@ class Solution {
                     res.add(sb.toString());// // 将拼接好的路径字符串添加到结果列表 res 中
                     return;
                 }
-                // 递归和回溯是同时进行，所以要放在同一个花括号里
+                // 递归和回溯是同时进行，所以要放在同一个花括号里。在递归调用返回后，从 paths 列表中移除当前节点的值，以恢复到调用该递归函数之前的状态
                 if (root.left != null) { // 左
                     traversal(root.left, paths, res);
                     paths.remove(paths.size() - 1);// 回溯
