73f887c67dec55970a9792768683d700a7ac96f8
Emily <emily.zy.lin@gmail.com>
2024-08-06 15:18:57 -0500

    add the 2nd solution of lc98

--- 98.validate-binary-search-tree.java
+++ 98.validate-binary-search-tree.java
@@ -49,6 +49,23 @@ class Solution {
     
 }
 
+//前序遍历
+// class Solution {
+//     public boolean isValidBST(TreeNode root) {
+//         return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+//     }
+
+//     private boolean isValidBST(TreeNode node, long left, long right) {
+//         if (node == null) {
+//             return true;
+//         }
+//         long x = node.val;
+//         return left < x && x < right &&
+//                isValidBST(node.left, left, x) &&
+//                isValidBST(node.right, x, right);
+//     }
+// }
+
 //后序遍历：
 // class Solution {
 //     public boolean isValidBST(TreeNode root) {
