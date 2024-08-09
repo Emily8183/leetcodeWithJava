528166f07799e3d7581fb0700ee8f21d13dd5c1f
Emily <emily.zy.lin@gmail.com>
2024-08-09 15:48:19 -0500

    finish lc108

--- 108.convert-sorted-array-to-binary-search-tree.java
+++ 108.convert-sorted-array-to-binary-search-tree.java
@@ -22,8 +22,23 @@
  */
 class Solution {
     public TreeNode sortedArrayToBST(int[] nums) {
+       return traversal(nums, 0, nums.length-1);
         
     }
+
+        // 左闭右闭区间[left, right]
+        private TreeNode traversal(int[]nums, int left, int right) {
+            if(left>right) return null;
+
+            int mid = left+((right-left)/2);
+
+            TreeNode root = new TreeNode(nums[mid]);
+
+            root.left = traversal(nums, left, mid-1);
+            root.right = traversal(nums, mid+1, right);
+
+            return root;
+        }
 }
 // @lc code=end
 
