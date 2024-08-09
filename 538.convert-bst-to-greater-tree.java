0d247ec0f91ee50ed392af3213624ad02ff32c9c
Emily <emily.zy.lin@gmail.com>
2024-08-09 17:46:42 -0500

    finish lc538

--- 538.convert-bst-to-greater-tree.java
+++ 538.convert-bst-to-greater-tree.java
@@ -21,9 +21,29 @@
  * }
  */
 class Solution {
+
+    int sum;
     public TreeNode convertBST(TreeNode root) {
+
+        sum =0;
         
+        convertBST1(root);
+        return root;
     }
+
+
+        private void convertBST1(TreeNode root) {
+            if(root == null) {
+                return;
+            }
+
+            // 反中序遍历
+            convertBST1(root.right);
+            sum+= root.val;
+            root.val=sum;
+            convertBST1(root.left);
+
+        }
 }
 // @lc code=end
 
