fc90de9e71607939c61af6f48634ab18ec657f9d
Emily <emily.zy.lin@gmail.com>
2024-08-27 10:05:58 -0500

    update lc704(another solution)

--- .idea/sandbox/leetcodeTest.java
+++ .idea/sandbox/leetcodeTest.java
@@ -3,37 +3,40 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
 
 public class leetcodeTest {
 
     public static void main(String[] args) {
         leetcodeTest leetcodeTest = new leetcodeTest();
-        int[] nums= {2,3,1,2,4,3};
-        int target = 7;
-   
-        Integer result = leetcodeTest.minSubArrayLen(target, nums) ;
+        int [] nums = {-1,0,3,5,9,12};
+        int target = 0;
+        int result = leetcodeTest.search(nums, target);
         System.out.println(result);
     }
-    public int minSubArrayLen(int target, int[] nums) {
-        //设定left和right指针，并把minLength定义为最大初始值
-        int left = 0;
-        int minLength = Integer.MAX_VALUE;
-        int sum = 0;
- 
-        //将minLength的值与每一次找到的窗口长度进行比较，取较小值
-        for(int right=0; right < nums.length; right++) {
-            sum += nums[right];
- 
-            while(sum >= target) {
-             minLength = Math.min(minLength, right-left+1); //先记录当下的minLength
-             sum-=nums[left]; //再不断调整sum与left
-             left++;
-            }
-         }
- 
-        //最后判断minLength是否等于Integer.MAX_VALUE，如果等于，说明没有找到满足条件的子数组，返回0，否则返回minLength
-        return minLength== Integer.MAX_VALUE? 0:minLength;
-     }
+    public int search(int[] nums, int target) {
+        //左闭右开
+        int left =0;
+        int right = nums.length;
+
+        while(left < right)
+
+        {
+           int mid = left + (right-left)/2; 
+
+           if(nums[mid] == target) 
+           {
+            return mid;
+           } else if(nums[mid] < target){
+            left = mid + 1;
+           } else {right = mid;}
+
+        }
+
+        return -1;
+    }
 }
     
 
---
+++ 515.find-largest-value-in-each-tree-row.java
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode id=515 lang=java
+ *
+ * [515] Find Largest Value in Each Tree Row
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+//使用BFS广度遍历，套模板，用stack解决问题
+
+
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        if(root==null) return new ArrayList<>();  
+        //在输入为空树的情况下，安全且合理地返回一个空的结果。如返回Null，调用该方法的代码在访问返回结果时，可能会抛出 NullPointerException
+
+        queue.offer(root);//先要把root放进queue内
+
+        while(!queue.isEmpty()) {
+            int size = queue.size(); 
+            //这里需要给queue.size()做一个快照，因为后面会不断pop，所以size会变
+            //size在while loop里是局部变量，但是对for loop来说是全局变量
+            //这里用maxValue来记录当前这一层的最大值
+            int maxValue = Integer.MIN_VALUE;
+
+            for(int i=0; i<size; i++) {
+                TreeNode node = queue.poll();
+                maxValue = Math.max(maxValue, node.val);
+
+                if(node.left != null) {queue.offer(node.left);}
+
+                if(node.right !=null) {queue.offer(node.right);}
+
+            }
+
+            result.add(maxValue);
+        }
+        return result;
+    }
+}
+// @lc code=end
+
