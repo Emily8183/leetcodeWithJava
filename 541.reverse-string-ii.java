9ab977b15eac846c0bbaf49aab283e7396424dad
Emily <emily.zy.lin@gmail.com>
2024-08-29 15:25:52 -0500

    redo lc541

--- 541.reverse-string-ii.java
+++ 541.reverse-string-ii.java
@@ -7,31 +7,62 @@
 // @lc code=start
 class Solution {
     public String reverseStr(String s, int k) {
+
+    
         char[] arr = s.toCharArray();
 
-        //在2k这一段反转，限制条件是2k没有超过arr.length
-        for(int i=0; i<arr.length; i+=2*k) {
-            if(i+k <= arr.length) {
-            reverse(arr,i,i+k-1);
-            } else{
-           //如果剩余字符少于k，把剩下的也反转
-           reverse(arr,i,arr.length-1);
+        for (int i = 0; i< arr.length; i+=2*k){
+
+            if(i+k <=arr.length) {
+                reverse(arr, i, i+k-1);
+            } else {
+                reverse(arr, i, arr.length-1);
             }
+
         }
+
         return new String(arr);
     }
 
-    private void reverse(char[] arr, int start, int end){
-        while (start >= end) {
-            return;
-        }
+    private void reverse(char[] arr, int start, int end) {
+        if(start >= end) return; 
 
-        char temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
+        while(start < end) { //二刷，递归中这里需要一个while loop
+            char temp = arr[end];
+            arr[end] = arr[start];
+            arr[start] = temp;
 
-        reverse(arr, start+1, end-1);
+            start++;
+            end--;
+        }}
+
     }
-}
+
+    //     char[] arr = s.toCharArray();
+
+    //     //在2k这一段反转，限制条件是2k没有超过arr.length
+    //     for(int i=0; i<arr.length; i+=2*k) {
+    //         if(i+k <= arr.length) {
+    //         reverse(arr,i,i+k-1);
+    //         } else{
+    //        //如果剩余字符少于k，把剩下的也反转
+    //        reverse(arr,i,arr.length-1);
+    //         }
+    //     }
+    //     return new String(arr);
+    // }
+
+    // private void reverse(char[] arr, int start, int end){
+    //     while (start >= end) {
+    //         return;
+    //     }
+
+    //     char temp = arr[start];
+    //     arr[start] = arr[end];
+    //     arr[end] = temp;
+
+    //     reverse(arr, start+1, end-1);
+    // }
+
 // @lc code=end
 
