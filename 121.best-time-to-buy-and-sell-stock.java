bc74b7bf44c761306cc131ddf1b9bd3c54075977
Emily <emily.zy.lin@gmail.com>
2024-08-30 15:51:03 -0500

    update notes of LC121

--- 121.best-time-to-buy-and-sell-stock.java
+++ 121.best-time-to-buy-and-sell-stock.java
@@ -3,6 +3,8 @@
  *
  * [121] Best Time to Buy and Sell Stock
  */
+// 做这道题需要注意，lowestNum始终是在prices[i]前面求得的，这是个逻辑先后顺序。因此我们找到的prices[i]一定在lowestNum后面，所得的相减的数字，要么始终保持是0（代表lowestprice后面的数一直递减）
+// 要么就是prices[i]-lowestNum之间的最大数
 
 // @lc code=start
 class Solution {
@@ -10,6 +12,7 @@ class Solution {
    
     public int maxProfit(int[] prices) {
 
+    //int不断更新，直到找到最大的profit
     int maxProfit = 0;
 
     //找到最低的购入点
