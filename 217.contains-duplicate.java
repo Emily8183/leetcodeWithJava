6834f87a71d144d8e8119b59919141796be6d29d
Emily <emily.zy.lin@gmail.com>
2024-09-04 10:41:27 -0500

    lc217, solution of hashset with notes

--- 217.contains-duplicate.java
+++ 217.contains-duplicate.java
@@ -7,7 +7,7 @@
 // @lc code=start
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        //解法1：双指针
+        //解法1：双指针, Time: O(n log n),Space: O(1)
         // Arrays.sort(nums);
 
         // for(int i=1;i<nums.length;i++) {
@@ -20,16 +20,27 @@ class Solution {
 
         //解法2：hashmap
 
-        Map<Integer,Integer> map = new HashMap<>();
+        // Map<Integer,Integer> map = new HashMap<>();
 
-        for(int num : nums) {
-            map.put(num, map.getOrDefault(num,0)+1);
+        // for(int num : nums) {
+        //     map.put(num, map.getOrDefault(num,0)+1);
 
-            if(map.get(num) > 1) return true;
-        }
+        //     if(map.get(num) > 1) return true;
+        // }
+
+        // return false;
+
+        //解法3：hashset, time: O(n), space: O(n)
+        Set<Integer> set = new HashSet<>();
+
+        for(int num:nums){
+
+            if(set.contains(num)) return true;//这行需要在.add()的前面
+
+           set.add(num);  
+        } 
 
         return false;
-        
     }
 }
 // @lc code=end
