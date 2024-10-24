52a2c0e48f5c2987c3c217d2f3b24b006fe7c700
Emily <emily.zy.lin@gmail.com>
2024-10-24 14:18:26 -0500

    add the bfs method to lc104

--- 111.minimum-depth-of-binary-tree.java
+++ 111.minimum-depth-of-binary-tree.java
@@ -22,25 +22,61 @@
  */
 class Solution {
     public int minDepth(TreeNode root) {
-        if(root == null) return 0;
-        //当节点为0时，没有高度，因此返回0
 
-        int leftHeight = minDepth(root.left);
-        int rightHeight = minDepth(root.right);
+        //递归法
+        // if(root == null) return 0;
+        // //当节点为0时，没有高度，因此返回0
+
+        // int leftHeight = minDepth(root.left);
+        // int rightHeight = minDepth(root.right);
 
         
-        if(root.left == null && root.right != null) {
-            return rightHeight + 1;
+        // if(root.left == null && root.right != null) {
+        //     return rightHeight + 1;
+        // }
+
+        // if(root.left != null && root.right == null) {
+        //     return leftHeight + 1;
+        // }
+
+        // //处理中间
+        // int minHeight = Math.min(leftHeight, rightHeight);
+
+        // return minHeight + 1;
+
+        //迭代法：
+        if (root == null) return 0;
+
+        if (root.left == null && root.right == null) return 1;
+
+        int depth = 0;
+
+        Queue<TreeNode> que = new LinkedList<>();
+
+        que.offer(root);
+
+        while (!que.isEmpty()) {
+            int len = que.size();
+            depth++;
+
+            while (len > 0) {
+
+                TreeNode temp = que.poll();
+
+                if (temp.left == null && temp.right == null) {
+                    return depth;
+                }
+
+                if (temp.left != null) que.offer(temp.left);
+                if (temp.right != null) que.offer(temp.right);
+
+                len--;
+            }
+
         }
 
-        if(root.left != null && root.right == null) {
-            return leftHeight + 1;
-        }
+        return depth;
 
-        //处理中间
-        int minHeight = Math.min(leftHeight, rightHeight);
-
-        return minHeight + 1;
         
     }
 }
