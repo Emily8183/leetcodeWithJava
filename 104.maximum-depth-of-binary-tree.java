d0147612620e77b32f3abc7788f9331450c77bb3
Emily <emily.zy.lin@gmail.com>
2024-10-24 11:11:40 -0500

    add bfs method for lc104

--- 104.maximum-depth-of-binary-tree.java
+++ 104.maximum-depth-of-binary-tree.java
@@ -22,20 +22,49 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
-        if(root == null) return 0;
+        //递归
+        // if(root == null) return 0;
         //节点为空时，高度为零
 
         //使用后序遍历法，左右中
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-        int maxHeight = Math.max(left,right) + 1;
+        // int left = maxDepth(root.left);
+        // int right = maxDepth(root.right);
+        // int maxHeight = Math.max(left,right) + 1;
         //确定单层递归的逻辑：
         //先求它的左子树的深度，再求右子树的深度，最后取左右深度最大的数值
         //再+1 （加1是因为算上当前中间节点）就是目前节点为根节点的树的深度。
 
-        return maxHeight;
+        // return maxHeight;
 
-        
+        //迭代法
+        if (root == null) return 0;
+
+        Queue<TreeNode> que = new LinkedList<>();
+
+        que.offer(root);
+
+        int depth = 0;
+
+        while (!que.isEmpty()) {
+
+            int len = que.size();
+
+            while (len > 0) {
+                TreeNode temp = que.poll();
+                    
+                    if (temp.left != null) {
+                        que.offer(temp.left);
+                    } 
+                    
+                    if (temp.right != null) {
+                        que.offer(temp.right);
+                    }
+                len--;
+            }
+            depth++;
+        }
+
+        return depth;
     }
 }
 // @lc code=end
