50e6418b3f2dc1f88078dd6f64a906b8ec5fa83a
Emily <emily.zy.lin@gmail.com>
2024-10-25 12:08:17 -0500

    add the traverse method to lc226

--- 226.invert-binary-tree.java
+++ 226.invert-binary-tree.java
@@ -22,16 +22,45 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        if ( root == null) return root;
+        //递归法
+        // if ( root == null) return root;
 
-        TreeNode temp = root.left;
-        root.left = root.right;
-        root.right = temp;
+        // TreeNode temp = root.left;
+        // root.left = root.right;
+        // root.right = temp;
 
-        invertTree(root.left);
-        invertTree(root.right);
+        // invertTree(root.left);
+        // invertTree(root.right);
+
+        // return root;
+
+        //迭代法
+
+        if (root == null) return root;
+
+        Queue<TreeNode> que = new LinkedList<>();
+
+        if (root != null) que.offer(root);
+
+        while(!que.isEmpty()) {
+            int len = que.size();
+            TreeNode temp = que.poll();
+            invert(temp);
+
+            if (temp.left != null) que.offer(temp.left);
+            if (temp.right != null) que.offer(temp.right);
+
+            len--;
+
+        }
 
         return root;
+    }
+
+    private void invert (TreeNode node) {
+        TreeNode temp = node.left;
+        node.left = node.right;
+        node.right = temp;
     }
 }
 // @lc code=end
