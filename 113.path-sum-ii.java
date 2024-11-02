b1c8b1f0a481b15fdf227f0fc0a36233d2d666df
Emily <emily.zy.lin@gmail.com>
2024-11-01 20:35:32 -0500

    optimize lc112

---
+++ 113.path-sum-ii.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=113 lang=java
+ *
+ * [113] Path Sum II
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> list = new ArrayList<>();
+
+        
+        
+    }
+}
+// @lc code=end
+
