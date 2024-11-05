d4e7bfa8b214a893f5e64ad26f3443a56536e170
Emily <emily.zy.lin@gmail.com>
2024-11-05 17:06:41 -0600

    re-solve lc700 by iterative method

--- 700.search-in-a-binary-search-tree.java
+++ 700.search-in-a-binary-search-tree.java
@@ -22,23 +22,38 @@
  */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
+
+        //递归法
        
-            if (root == null || root.val == val) {
+            // if (root == null || root.val == val) {
+            //     return root;
+            // }
+
+            // TreeNode result = null;
+
+            // //利用二叉搜索树的特性，如果val<root.val，向左搜索，反之，向右搜索。
+            // if (val < root.val) {
+
+            //     result = searchBST(root.left, val);
+            
+            // } else {
+            //     result = searchBST(root.right, val);
+            // }
+
+            // return result;
+
+        //迭代法：充分利用二叉搜索树的特性
+        while (root != null) {
+            if (root.val < val) {
+                root = root.right;
+            } else if (root.val > val) {
+                root = root.left;
+            } else if (root.val == val) {
                 return root;
             }
+        }
 
-            TreeNode result = null;
-
-            //利用二叉搜索树的特性，如果val<root.val，向左搜索，反之，向右搜索。
-            if (val < root.val) {
-
-                result = searchBST(root.left, val);
-            
-            } else {
-                result = searchBST(root.right, val);
-            }
-
-            return result;
+        return null;
         
     }
         
