dd5b084ae4139ecf3efd49d54e272d3e68a11b38
Emily <emily.zy.lin@gmail.com>
2025-02-07 22:20:30 -0600

    lc20, 34mins

--- 20.valid-parentheses.java
+++ 20.valid-parentheses.java
@@ -2,35 +2,56 @@
  * @lc app=leetcode id=20 lang=java
  *
  * [20] Valid Parentheses
+ Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+
+condition: String s = "", '(', ')', '{', '}', '[' and ']'. for example：s = "()[]{}", s = "([])"
+goal: isValid: t/f
+solution:stack
+
+Syntax: s.charAt(i), char ''，not ""
+ * 
  */
 
 // @lc code=start
+
+import java.util.Stack;
+
 class Solution {
     public boolean isValid(String s) {
 
-        Deque<Character> stack = new LinkedList<>(); //双端队列
+        //edge
+        if (s.length() == 1) return false;
 
-        if (s.length() %2 !=0){
-            return false;
-        }
+        Stack<Character> stack = new Stack<>();
 
-        for (int i=0; i < s.length(); i++ ) {
-            char ch = s.charAt(i);
+        for (int i = 0; i <= s.length()-1; i++) {
 
-            if(ch == '(') {
+            char c = s.charAt(i);
+
+            if (c == '(') {
                 stack.push(')');
-            } else if (ch=='{') {
-                stack.push('}') ;
-            } else if (ch == '[') {
+            } else if (c == '[') {
                 stack.push(']');
-            } else if (stack.isEmpty() || stack.peek() != ch) {
-                return false;
+            } else if (c == '{') {
+                stack.push('}');
             } else {
+                if (stack.isEmpty() || c != stack.peek() ) return false;
+
                 stack.pop();
-            } 
+            }
+
+   
+
         }
 
         return stack.isEmpty();
+
         
     }
 }
