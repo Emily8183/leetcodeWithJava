3a3015fe135e1693f5435daaa0403fa7dbe7c609
Emily <emily.zy.lin@gmail.com>
2025-02-10 16:13:34 -0600

    finish lc278

--- 278.first-bad-version.java
+++ 278.first-bad-version.java
@@ -2,6 +2,15 @@
  * @lc app=leetcode id=278 lang=java
  *
  * [278] First Bad Version
+ * condition: int[] versions;
+ * goal: find the first bad version
+ * solution: binary search [t,t,t,t,t,t,f,f,f,f,f,f]
+ * left = true, mid = false; 
+ * 
+ * [product 1, product 2, product3...]
+ * index = 0, 1, 2
+ * 
+ * while (left < right) 
  */
 
 // @lc code=start
@@ -9,29 +18,24 @@
       boolean isBadVersion(int version); */
 
 public class Solution extends VersionControl {
-
-    //左闭右闭, 二分查找
-
     public int firstBadVersion(int n) {
 
-        int start = 0;
-        int end = n-1; //length是n，index是n-1
-        int mid = n/2;
+        int left = 1;
+        int right = n;
 
-        //应该用while的时候不要用if
-        //设定左闭右闭区间，所以这里有等号
-        while (start <= end) { //这里是遍历的终止条件
-            mid = start + (end-start)/2;
+        while (left <= right) { //left inclusive, right inclusive
 
-            if(!isBadVersion(mid)) {
-            start = mid + 1;
+            int mid = left + (right-left)/2; 
+
+            if (!isBadVersion(mid)) {
+                left = mid + 1;
             } else {
-            end = mid - 1;
+                right = mid - 1;
             }
-
         }
 
-        return start; //edge case: 当只有一个元素的时候，start = mid +1，返回的false index是1，因此也是成立的
+        return left;
+
     }
 
 }
