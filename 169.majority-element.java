03e9317e2c15722ba1ba34afbe7cf2a4368f7294
Emily <emily.zy.lin@gmail.com>
2025-02-11 15:29:49 -0600

    lc169 boyer moore

--- 169.majority-element.java
+++ 169.majority-element.java
@@ -6,7 +6,7 @@
  * goal: return the elements that shows more than n/2
  * 
  * solution: 
- * hashmap
+ * 1) hashmap  2)Boyer-moore
  */
 
 // @lc code=start
@@ -16,21 +16,42 @@ import java.util.HashMap;
 class Solution {
     public int majorityElement(int[] nums) {
 
-        int n = nums.length;
+        //hashmap
+        // int n = nums.length;
        
-        HashMap<Integer, Integer> map = new HashMap<>();
+        // HashMap<Integer, Integer> map = new HashMap<>();
         
-        //to store the freq
-        for (int i = 0; i < n; i++) {
+        // //to store the freq
+        // for (int i = 0; i < n; i++) {
             
-            map.put(nums[i], map.getOrDefault(nums[i],0)+1); //key = nums[i], value = freq
+        //     map.put(nums[i], map.getOrDefault(nums[i],0)+1); //key = nums[i], value = freq
 
-            if (map.get(nums[i]) > n/2) {
-                return nums[i];
+        //     if (map.get(nums[i]) > n/2) {
+        //         return nums[i];
+        //     }
+        // }
+
+        // return -1;
+
+        //boyer-moore
+
+        int count = 0;
+        Integer candidate = null;
+
+        for (int num: nums) {
+            if (count == 0) candidate = num;
+
+            if (num == candidate) {
+                count += 1;  // 遇到相同的数字，增加 count
+            } else {
+                count -= 1;  // 遇到不同的数字，减少 count
             }
+
+            // count += (num == candidate) ? 1 : -1;
+
         }
 
-        return -1;
+        return candidate;
 
     }
 
