9873d75af8b5ff6f421cf524d5d84d75f0a18f54
Emily <emily.zy.lin@gmail.com>
2025-03-08 16:05:24 -0600

    lc42

--- 42.trapping-rain-water.java
+++ 42.trapping-rain-water.java
@@ -15,39 +15,40 @@
 // @lc code=start
 class Solution {
     public int trap(int[] height) {
-        int peakIndex = 0;
-        int water = 0;
+       //for loop, find the highest height
+       int peakIndex = 0;
+       int water = 0;
 
-        for (int i = 0; i < height.length; i++) {
-           if (height[i] > height[peakIndex]) {
-            peakIndex = i;
-           }
-        }
-
-        //traverse from leftMost index to the peak index, [)
-        int leftMost = 0;
-
-        for (int i = 0; i < peakIndex; i++) {
-            if (height[leftMost] > height[i]) {
-                water = water + height[leftMost] - height[i];
-            } else {
-                leftMost = i;
+       for (int i = 0; i < height.length; i++) {
+            if (height[i] > height[peakIndex]) {
+                peakIndex = i;
             }
-        }
+       }
 
-        //traverse from RightMost index to the peak index, (]
-        int rightMost = height.length - 1;
+       int leftMost = 0;
 
-        for (int i = height.length - 1; i > peakIndex; i--) {
+       //loop the left side
+       for (int i = 0; i < peakIndex; i++) {
+
+           if (height[leftMost] > height[i]) {
+                water = water + height[leftMost] - height[i];
+           } else {
+                leftMost = i;
+           }
+       }
+
+       int rightMost = height.length -1;
+
+       for (int i = height.length-1; i > peakIndex; i--) {
             if (height[rightMost] > height[i]) {
                 water = water + height[rightMost] - height[i];
             } else {
                 rightMost = i;
             }
 
-        }
+       }
 
-        return water;
+       return water;
     }
 }
 // @lc code=end
