a088c6ef148763c99b89ca8e6d53791f16a342e5
Emily <emily.zy.lin@gmail.com>
2025-03-08 14:57:10 -0600

    lc 125, 209, 283

--- 14.longest-common-prefix.java
+++ 14.longest-common-prefix.java
@@ -1,27 +1,30 @@
 /*
  * @lc app=leetcode id=14 lang=java
- *goal: to find the longest prefix
- solution: assuming the longest is the first word, chop off the letters dont match
+ 
  * [14] Longest Common Prefix
+ * goal: find the longest common prefix string 
+ * 1) edge case: array is empty
+ * 2) 
  */
 
 // @lc code=start
 class Solution {
     public String longestCommonPrefix(String[] strs) {
 
-        if (strs.length == 0 || strs == null) return "";
+        if (strs == null || strs.length == 0) return "";
 
         String prefix = strs[0];
 
-        for (int i = 1; i < strs.length; i++) {
+        for (int i = 1; i <= strs.length-1; i++) {
 
-           while (!strs[i].startsWith(prefix)) { 
-                    prefix = prefix.substring(0, prefix.length()-1);
-                }
+            while(!strs[i].startsWith(prefix)) {
+                prefix = prefix.substring(0, prefix.length()-1);
+            }
         }
-        
 
         return prefix;
+
+     
         
     }
 }
--- 283.move-zeroes.java
+++ 283.move-zeroes.java
@@ -8,39 +8,23 @@
 class Solution {
     public void moveZeroes(int[] nums) {
 
-        int j=0;//j指向非零数字的index
+        int slow = 0; 
 
-        //双指针, 不需要嵌套两个循环, j++
-        for(int i=0;i<nums.length;i++){
-            // for(int j=0;j<nums.length;j++){
-                if(nums[i]!=0) {
-                    nums[j++]=nums[i];
+        for (int fast = 0; fast < nums.length; fast++) {
 
-                } 
+            if (nums[fast] != 0) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+           
         }
 
-        for(int i= j;i<nums.length; i++){
-            //注意这里i的值，nums = [0,1,0,3,12]这个例子中，slow的指针是停在3而不是2，因此i = slow，而不是slow+1
-            nums[i]=0;
+        for (int i = slow; i < nums.length; i++) {
+            nums[i] = 0;
         }
+       
     }
 }
-
-// 打印数组结果不能直接用System.out.println(nums),打印的是数组对象的引用，返回类似 [I@1b6d3586 这样的哈希值。
-// 为了正确打印数组的内容，需要遍历数组并打印每个元素，或者使用 Arrays.toString(nums) 来将数组转换为字符串表示，再打印出来。
-    // public static void main(String[] args) {
-    // leetcodeTest leetcodeTest = new leetcodeTest();
-    // int[] nums = {0,1,0,3,12}; 
-   
-    // leetcodeTest.moveZeroes(nums);
-
-    // for(int num:nums) {
-    //     System.out.println(num);
-    // }
-
-    //OR:  System.out.println(Arrays.toString(nums)); 
-
-// }
 
 // @lc code=end
 
