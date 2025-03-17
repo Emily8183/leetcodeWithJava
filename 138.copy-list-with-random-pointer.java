1177c79bb5bcc4c8e3a8f5584172782e44fb88b8
Emily <emily.zy.lin@gmail.com>
2025-03-16 23:10:59 -0500

    finish lc138

---
+++ 138.copy-list-with-random-pointer.java
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode id=138 lang=java
+ *
+ * [138] Copy List with Random Pointer
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
+        Map<Node, Node> map = new HashMap<>();
+        Node cur = head;
+
+        //step1: copy the nodes' value only
+        while (cur != null) {
+            map.put(cur, new Node(cur.val, null, null));
+            cur = cur.next;
+        }
+
+        //step2: add on the next pointer and random pointer
+        cur = head;
+
+        while (cur != null) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+
+            cur = cur.next;
+        }
+
+        return map.get(head); //注意返回值
+        
+    }
+}
+// @lc code=end
+
