2ca535bdd0df7ad1dc2f105ab2c721bf1f6d68c8
Emily <emily.zy.lin@gmail.com>
2025-04-12 22:28:17 -0500

    first solution to lc543

--- 543.diameter-of-binary-tree.java
+++ 543.diameter-of-binary-tree.java
@@ -20,37 +20,40 @@
  *     }
  * }
  * solution:
- * 左最深+右最深=最长，
+ * 左最高+右最高=最长，
  * 3种情况的比较，a)经过根节点，b)左子树某节点，c)右子树某节点
  * 注意：在子树中（当root是在子节点上，而不是根节点）也有可能出现蕞大值，这个情况要考虑进去。所以这题虽然简单，其实有个陷阱。
  * 
  * 1）return的结果，要用Math.max进行比较
  * 2）往下递归的时候，深度如何叠加，return 1 + Math.max(getHeight(root.left), getHeight(root.right));
- *
+ 
  * 
  */
 class Solution {
+
+    int max = 0; //所以max放在这一行，作为class成员变量
+
     public int diameterOfBinaryTree(TreeNode root) {
 
-        if (root == null) return 0;
+        //int max = 0; //int是基本类型的值，所以即使作为参数传到dfs, dfs()只是复制了一个副本，不影响原来的max，所以不要传
 
-        int leftTreeHeight = getHeight(root.left);
-        int rightTreeHeight = getHeight(root.right);
-        int maxLeftHeight = diameterOfBinaryTree(root.left);
-        int maxRightHeight = diameterOfBinaryTree(root.right);
-
-        return Math.max(leftTreeHeight + rightTreeHeight, Math.max(maxLeftHeight,maxRightHeight));
+        dfs(root);
+        return max;
 
     }
 
-    private int getHeight(TreeNode node) {
+    private int dfs(TreeNode node) {
         if (node == null) return 0;
 
-        int leftHeight = getHeight(node.left);
-        int rightHeight = getHeight(node.right);
+        int leftHeight = dfs(node.left);
+        int rightHeight = dfs(node.right);
 
-        return 1 + Math.max(leftHeight, rightHeight);
+        max = Math.max(leftHeight + rightHeight, max);
+
+
+        return Math.max(leftHeight, rightHeight) + 1;
     }
 }
+
 // @lc code=end
 
